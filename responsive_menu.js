798bb8ee5564697a1c1194fa7d4ca7dae522ec2d
Gunjesh Kumar <gunjeshk34@gmail.com>
2024-09-03 22:16:42 +0530

    Delete responsive_menu.js

--- responsive_menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(document).ready(function(){
-        $(".hamburger").click( function() {
-            $(".mobile-menu").slideToggle();
-        });
-        
-    });
-});
